95acba139505ca78c423536ea58ec489bb40179d
shiro76 <kololi76@gmail.com>
2022-04-22 19:42:08 +0200

    first commit

--- backend/app.js
+++ backend/app.js
@@ -14,15 +14,6 @@ mongoose.connect('mongodb+srv://test:pokemon76@cluster0.31ydm.mongodb.net/test?r
 
 const app = express();
 
-
-// Ces headers permettent :
-
-// d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
-
-// d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
-
-// d'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
